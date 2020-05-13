95d22f015e0a7e1dd3bd86211d2cdff5ab7f5a7e
“vishnu <vishnu@bflgroup.ae>
2020-05-13 08:13:48 +0400

    Updates

---
+++ precache-manifest.c7fe74214c7cee0f410312cdba96836a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d575f82bcb939d3d03ebb27e7be8036f",
+    "url": "/vishnuvinayan/index.html"
+  },
+  {
+    "revision": "16de105936ad5e0e9b0c",
+    "url": "/vishnuvinayan/static/css/2.5cc647d9.chunk.css"
+  },
+  {
+    "revision": "5fcaccd548d73ff28c50",
+    "url": "/vishnuvinayan/static/css/main.e9c2f375.chunk.css"
+  },
+  {
+    "revision": "16de105936ad5e0e9b0c",
+    "url": "/vishnuvinayan/static/js/2.609d56eb.chunk.js"
+  },
+  {
+    "revision": "5fcaccd548d73ff28c50",
+    "url": "/vishnuvinayan/static/js/main.2ce41858.chunk.js"
+  },
+  {
+    "revision": "f2276834de1ef28f53b6",
+    "url": "/vishnuvinayan/static/js/runtime~main.2112c7c4.js"
+  },
+  {
+    "revision": "7c33af2f1a5b73e5d6884ea1846a054d",
+    "url": "/vishnuvinayan/static/media/boost-your-post-for-increasing-sales.7c33af2f.md"
+  },
+  {
+    "revision": "9f553f26846bbd2553a9ed2e3a604ed6",
+    "url": "/vishnuvinayan/static/media/difference-between-gatsbyjs-and-nextjs.9f553f26.md"
+  },
+  {
+    "revision": "8cc16894642414291f3700ac5396c1ae",
+    "url": "/vishnuvinayan/static/media/future-of-javascript.8cc16894.md"
+  },
+  {
+    "revision": "94fbd3842288f7042fb36a2b025fb7a5",
+    "url": "/vishnuvinayan/static/media/how-to-choose-javascript-framework-for-project.94fbd384.md"
+  },
+  {
+    "revision": "6566a29212d29ce3455d349471c4c4c3",
+    "url": "/vishnuvinayan/static/media/how-to-get-10k-instagram-followers.6566a292.md"
+  },
+  {
+    "revision": "368cc11c0fc1f5868a2461a538b6dd81",
+    "url": "/vishnuvinayan/static/media/installing-nodejs-on-your-device.368cc11c.md"
+  },
+  {
+    "revision": "c7fe48e63067433331f84ec94e2e49fc",
+    "url": "/vishnuvinayan/static/media/markdown-html-supported-blog.c7fe48e6.md"
+  },
+  {
+    "revision": "d4881e738d4008ebb0ad0ed9e6587da5",
+    "url": "/vishnuvinayan/static/media/popular-javascript-library-in-2020.d4881e73.md"
+  },
+  {
+    "revision": "d490be272abdf01ca26f90685cec9b99",
+    "url": "/vishnuvinayan/static/media/programmers-must-read-books.d490be27.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/vishnuvinayan/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/vishnuvinayan/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/vishnuvinayan/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/vishnuvinayan/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c0c0a9c9cc01fca9adedff7998f6ac05",
+    "url": "/vishnuvinayan/static/media/think-out-of-the-box.c0c0a9c9.md"
+  },
+  {
+    "revision": "ea70d882cb4362f1ca2c8db077b78794",
+    "url": "/vishnuvinayan/static/media/time-to-use-latest-technology-for-creating-a-website.ea70d882.md"
+  },
+  {
+    "revision": "8e1efcf25b01edbf3d60df3f09646750",
+    "url": "/vishnuvinayan/static/media/trending-designs-in-2020.8e1efcf2.md"
+  },
+  {
+    "revision": "c7973cd5f0da590119aa22cd3147a84f",
+    "url": "/vishnuvinayan/static/media/uiux-design-starter-with-adobe-xd.c7973cd5.md"
+  },
+  {
+    "revision": "a1c7c37f05e57483b9ecb3edbdf5e5a3",
+    "url": "/vishnuvinayan/static/media/web-automation-with-python-language.a1c7c37f.md"
+  },
+  {
+    "revision": "40686a4e31c7336491e3abce057010eb",
+    "url": "/vishnuvinayan/static/media/what-nodejs-can-do.40686a4e.md"
+  }
+]);
